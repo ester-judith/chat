dd760c270cee3677c003c28c68766484531ae211
ester-judith <ester.j.ejcc@gmail.com>
2024-05-26 04:23:54 -0600

    first commit

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,132 @@
+import { useEffect, useRef, useState } from "react"
+import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
+
+const Chat = () => {
+    const [open,setOpen] = useState(false)
+    const [text, setText] = useState("")
+
+    const endRef = useRef(null)
+
+    useEffect(() => {
+        endRef.current?.scrollIntoView({ behavior : "smooth"});
+    }, []);
+
+    const handleEmoji = e => {
+        setText((prev) => prev + e.emoji)
+        setOpen(false)
+    }
+
+    console.log(text)
+
+    return (
+        <div className='chat'>
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>Lorem Ipsum dolor, sit amet</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
+                </div>
+            </div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsun
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            Lorem ipsun
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsun
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            Lorem ipsun
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsun
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            Lorem ipsun
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsun
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <img src="https://cdn.domestika.org/c_limit,dpr_1.0,f_auto,q_80,w_820/v1485276242/content-items/001/838/882/captura2-original.png?1485276242" alt="" />
+                        <p>
+                            Lorem ipsun
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div ref={endRef}></div>
+            </div>
+            <div className="botton">
+                <div className="icons">
+                    <img src="./img.png" alt="" />
+                    <img src="./camera.png" alt="" />
+                    <img src="./mic.png" alt="" />
+                </div>
+                <input type="text" placeholder="Type a message..."
+                value={text} 
+                onChange={e=>setText(e.target.value)}/>
+                <div className="emoji">
+                    <img src="./emoji.png" alt="" 
+                    onClick={() => setOpen((prev) => !prev)}/>
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,72 @@
+import "./detail.css"
+
+const Detail = () => {
+    return (
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane Doe</h2>
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & Help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared photos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://i.pinimg.com/736x/d0/dd/2c/d0dd2c20cbf8a1230128b5c20f13775c.jpg" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://i.pinimg.com/736x/d0/dd/2c/d0dd2c20cbf8a1230128b5c20f13775c.jpg" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://i.pinimg.com/736x/d0/dd/2c/d0dd2c20cbf8a1230128b5c20f13775c.jpg" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                        <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+                <button className="lagout">Logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default Detail
